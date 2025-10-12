3c04e42c2dfbcb44ca2d735440340730abfbcc2a
capanator <160444832+Capanator@users.noreply.github.com>
2025-10-12 14:48:00 -0700

    Added Starter code for C1 Project Abstract Games

---
+++ C1_AbstractStrategyGames/Testing.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+public class Testing {
+    @Test
+    @DisplayName("STUDENT TEST CASE - Win Condition")
+    public void firstCaseTest() {
+        AbstractStrategyGame g = /* TODO - construct your game*/;
+
+    }
+
+    @Test
+    @DisplayName("STUDENT TEST CASE - Illegal Move")
+    public void secondCaseTest() {
+        AbstractStrategyGame g = /* TODO - construct your game*/;
+
+    }
+}
