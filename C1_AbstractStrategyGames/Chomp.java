ec3e5df3d3bc20016e876d6aaeb7546da234bd9a
capanator <160444832+Capanator@users.noreply.github.com>
2025-10-12 22:25:02 -0700

    fixed some row and col logic issues

--- C1_AbstractStrategyGames/Chomp.java
+++ C1_AbstractStrategyGames/Chomp.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Chomp extends AbstractStrategyGame {
     private int[][] chompBar;
-    private final int totalCols; 
     private final int totalRows;
+    private final int totalCols; 
 
     public static final int PLAYER_1 = 1;
     public static final int PLAYER_2 = 2;
@@ -18,12 +18,12 @@ public class Chomp extends AbstractStrategyGame {
      * Parameters: N/A
      */
     public Chomp() {
-        this.totalRows = 7;
-        this.totalCols = 4;
+        this.totalRows = 4;
+        this.totalCols = 7;
 
-        this.chompBar = new int[this.totalCols][this.totalRows];
-        for (int i = 0; i < this.totalCols; i++) {
-            for (int j = 0; j < this.totalRows; j++) {
+        this.chompBar = new int[this.totalRows][this.totalCols];
+        for (int i = 0; i < this.totalRows; i++) {
+            for (int j = 0; j < this.totalCols; j++) {
                 chompBar[i][j] = 3;
             }
         }
@@ -34,7 +34,7 @@ public class Chomp extends AbstractStrategyGame {
 
     /**
      * Behavior:
-     * Exceptions:
+     * Exceptions: 
      * Returns:
      * Parameters:
      */
@@ -46,26 +46,31 @@ public class Chomp extends AbstractStrategyGame {
     /**
      * Behavior: 
      * Exceptions:
-     * Returns: 
-     * Parameters:
+     * Returns:
+     *  - String: Representation, with top row representing 
+     *  - Ex: "| X | 0 | 1 | 2 |
+               -----------------
+               | 0 | 3 | 3 | 3 |
+               | 1 | 3 | 3 | 3 |""
+     * Parameters: N/A
      */
     public String toString() {
         //add the initial row labels for the chomp bar coordinate plane
         String currentChompBar = "| X |";
-        for (int i = 0; i < totalRows; i++) {
+        for (int i = 0; i < totalCols; i++) {
             currentChompBar += " " + i + " |";
         }
         currentChompBar += "\n";
 
         currentChompBar += "-----";
-        for (int i = 0; i < totalRows; i++) {
+        for (int i = 0; i < totalCols; i++) {
             currentChompBar += "----";
         }
         currentChompBar += "\n";
 
         //add the column labels as well as 
-        for (int i = 0; i < totalCols; i++) {
-            for (int j = 0; j < totalRows; j++) {
+        for (int i = 0; i < totalRows; i++) {
+            for (int j = 0; j < totalCols; j++) {
                 if (j == 0) {
                     currentChompBar += "| " + i + " |";
                 }
@@ -117,12 +122,12 @@ public class Chomp extends AbstractStrategyGame {
         
         System.out.print("Chomp Coordinate? ");
 
-        // Read the two integers for column and row
-        int col = input.nextInt();
+        // Read the two integers for rol and column
         int row = input.nextInt();
+        int col = input.nextInt();
         
         // Format them into a "<row> <col>" string
-        return col + " " + row;
+        return row + " " + col;
     }
 
     /**
@@ -140,15 +145,17 @@ public class Chomp extends AbstractStrategyGame {
         int row = Integer.parseInt(coordinates[0]);
         int col = Integer.parseInt(coordinates[1]);
         if (col < 0 || row < 0 || col >= totalCols || row >= totalRows) {
-            throw new IllegalArgumentException("Col and Row inputs must be within board bounds");
+            throw new IllegalArgumentException("Row and Col inputs must be within board bounds");
         }
         if (chompBar[row][col] == 0) {
             throw new IllegalArgumentException("That position is already empty, choose another");
         }
 
-        for (int i = col; i < totalCols; i++) {
-            for (int j = row; j < totalRows; j++) { 
-                if (chompBar[i][j] > 0) {
+        int currentChompLayer = chompBar[row][col]; 
+
+        for (int i = row; i < totalRows; i++) {
+            for (int j = col; j < totalCols; j++) { 
+                if (chompBar[i][j] == currentChompLayer) {
                     chompBar[i][j]--;
                 }
             }
